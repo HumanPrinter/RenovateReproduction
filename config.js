372b1c455879a9a895121a20861fbb0993690cd7
Oscar Brouwer <HumanPrinter@users.noreply.github.com>
2023-12-06 15:04:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     platform: 'github',
-    endpoint: 'https://github.com/HumanPrinter/RenovateReproduction/',
+    endpoint: 'https://github.com/HumanPrinter/',
     token: process.env.TOKEN,
     repositories: [
-        'PublicRepos/RenovateReproduction'
+        'RenovateReproduction'
     ]
   };
